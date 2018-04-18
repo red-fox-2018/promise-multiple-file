60fc4fae04e9e0c48cfefc89ec44ba64de42faff
fastabiqul1993 <fastabiqul1993@gmail.com>
2018-04-18 17:39:25 +0700

    done

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function readFilePromise() {
+function readFilePromise(file) {
   // psst, the promise should be around here...
+  return new Promise(function(resolve, reject){
+    fs.readFile(file,'utf-8',(err,data) => {
+      if(err){
+        reject(err)
+      }else {
+        let dataJSON = JSON.parse(data)
+        resolve(dataJSON)
+      }
+    })
+  })
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+  readFilePromise(parentFileName)
+  .then(parentFile => {
+    sleep.sleep(5)
+    readFilePromise(childrenFileName)
+    .then(childrenFile => {
+      for (let i = 0; i < parentFile.length; i++) {
+        
+        parentFile[i].children = []
+        for (let j = 0; j < childrenFile.length; j++) {
+          
+          if(parentFile[i].last_name == childrenFile[j].family) {
+            parentFile[i].children.push(childrenFile[j].full_name)
+          }
+        }
+      }
+
+      console.log(parentFile);
+    })
+    .catch(errChildren => {
+      console.log(`Terjadi error pada proses pembacaan data`);
+      console.log(errChildren);
+    })
+  })
+  .catch(errParent => {
+    console.log(`Terjadi error pada proses pembacaan data`);
+    console.log(errParent);
+  })
 }
 
 matchParentsWithChildrens('./parents.json', './childrens.json');
