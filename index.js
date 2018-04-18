d7bf2ddda8cbc67bf27e7b29ebb2a80b5ac06e24
violerine <viosefferine@gmail.com>
2018-04-18 18:08:18 +0700

    multiple file promise

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 const fs = require('fs');
-var sleep = require('sleep');
+// var sleep = require('sleep');
 
-function readFilePromise() {
-  // psst, the promise should be around here...
+function readFilePromise(input) {
+  let promise=new Promise((resolve,reject)=>{
+    fs.readFile(input,'utf8',function(err,data){
+        if(err) console.log(err)
+        else if(input==undefined) reject(err)
+        resolve(JSON.parse(data))
+    })
+})
+  return promise
 }
+
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
-  // your code here... (p.s. readFilePromise function(s) should be around here..)
+  var readParent= readFilePromise(parentFileName)
+  var readChildren=readFilePromise(childrenFileName)
+  Promise.all([readParent,readChildren])
+  .then(result => {
+    var parent = result[0]
+    var children=result[1]
+
+   for(var i=0; i<parent.length; i++){
+     parent[i]["children"]=[]
+     for(var j=0; j<children.length; j++){
+      if(children[j].family==parent[i].last_name){
+        parent[i].children.push(children[j].full_name)
+      }
+     }
+   }
+   
+   return parent
+  })
+  .then((parent)=>{
+    console.log(parent)
+  })
+  .catch(reject=>{
+    console.log(reject)
+  })
+ 
+
 }
 
-matchParentsWithChildrens('./parents.json', './childrens.json');
-console.log("Notification : Data sedang diproses !");
+// matchParentsWithChildrens('./parents.json', './childrens.json');
+// console.log("Notification : Data sedang diproses !");
 
 // for Release 2
 matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
